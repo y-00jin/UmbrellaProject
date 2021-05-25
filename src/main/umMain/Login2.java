acc627bc6c22729820881a43d39cded96465a1db
0206cho <68586044+0206cho@users.noreply.github.com>
2021-05-26 00:22:49 +0900

    민솔 깃 로그인 구현

--- src/main/umMain/Login2.java
+++ src/main/umMain/Login2.java
@@ -1,8 +1,9 @@
 package main.umMain;
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.FlowLayout;
-import java.awt.Image;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -17,65 +18,72 @@ import javax.swing.JTextField;
 
 import main.umRental.Rental_DB;
 
-public class Login extends JFrame implements ActionListener {
+public class Login2 extends JFrame implements ActionListener {
 
-	private JPanel p0, p1;
+	private JPanel p0, p1, p3;
 	private JLabel lbl_ID, lbl_PW;
 	private JTextField tf_ID, tf_PW;
 	private JButton btn_login, btn_exit;
 
-	public Login() { // 생성자
+	public Login2() { // 생성자
 		setTitle("관리자 로그인");
 		//setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		dispose();
-		setSize(600, 200);		
+		setSize(900, 700);		
 		setLocationRelativeTo(this);
 		//setResizable(false); // 프레임 창 사이즈 고정
 
 		// 이미지 레이블 생성
 		p0 = new JPanel();
-		p0.setLayout(new BorderLayout());
+		p0.setLayout(new GridLayout(1, 1, 0, 0));
 		p0.setBackground(Color.WHITE);
-		p0.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-		add(p0, BorderLayout.WEST);
+		p0.setBorder(BorderFactory.createEmptyBorder(30, 300, 0, 300));
+		add(p0, BorderLayout.NORTH);
 		
-		ImageIcon icon = new ImageIcon("libs/LoginImg.jpg"); //이미지아이콘 객체를 생성
-		
-		Image img = icon.getImage(); //변경할이미지 = 변경할아이콘.getImage(); 이미지아이콘을 이미지로 변환
-		Image changeImg = img.getScaledInstance(200, 150, Image.SCALE_SMOOTH);
-		//추출된 이미지의 크기를 조절하여 새로운 이미지 객체 생성
-		ImageIcon changeIcon = new ImageIcon(changeImg);
-		//새로운 이미지로 이미지아이콘 객체를 생성
-		JLabel imageLabel = new JLabel(changeIcon);
-		//JLabel imageLabel = new JLabel(icon);
+		ImageIcon icon = new ImageIcon("libs/user.png");
+		JLabel imageLabel = new JLabel(icon);
+		imageLabel.setHorizontalAlignment(JLabel.CENTER);
 		p0.add(imageLabel);
-		
+//		
 		p1 = new JPanel();
 		p1.setBackground(Color.WHITE);
-		p1.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 20));
-		p1.setBorder(BorderFactory.createEmptyBorder(30, 10, 10, 10));
-		add(p1);
+		p1.setLayout(new GridLayout(6, 1, 0, 20));
+
+	//	p1.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 20));
+		p1.setBorder(BorderFactory.createEmptyBorder(0, 300, 30, 300));
+		add(p1, BorderLayout.CENTER);
 		
-		
-		lbl_ID = new JLabel("ID: ");
+		lbl_ID = new JLabel("ID ");
+		lbl_ID.setFont(new Font("", Font.BOLD, 20));
 		p1.add(lbl_ID);
 		
-		tf_ID = new JTextField(10); // 창의 열 개수 10
+		tf_ID = new JTextField(); // 창의 열 개수 10
 		p1.add(tf_ID);
 		
-		lbl_PW = new JLabel("PW: ");
+		lbl_PW = new JLabel("PW ");
+		lbl_PW.setFont(new Font("", Font.BOLD, 20));
 		p1.add(lbl_PW);
 		
-		tf_PW = new JTextField(10); // 창의 열 개수 10
+		tf_PW = new JTextField(); // 창의 열 개수 10
 		p1.add(tf_PW);
+		
+//		p3 = new JPanel();
+//		p3.setBackground(Color.WHITE);
+//		p3.setLayout(new GridLayout(2, 1, 0, 30));
+//	//	p1.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 20));
+//		p3.setBorder(BorderFactory.createEmptyBorder(30, 300, 30, 300));
+//		add(p3, BorderLayout.SOUTH);
 		
 		btn_login = new JButton("로그인");
 		btn_login.setBackground(new Color(0xD4F4FA));
+		btn_login.setFont(new Font("휴먼둥근헤드라인", Font.PLAIN, 15));
+		
 		btn_login.addActionListener(this);
 		p1.add(btn_login);
 		
 		btn_exit = new JButton("종료");
 		btn_exit.setBackground(new Color(0xD4F4FA));
+		btn_exit.setFont(new Font("휴먼둥근헤드라인", Font.PLAIN, 15));
 		btn_exit.addActionListener(this);
 		p1.add(btn_exit);
 		
@@ -83,7 +91,7 @@ public class Login extends JFrame implements ActionListener {
 	}
 
 	public static void main(String[] args) {
-		new Login();
+		new Login2();
 	}
 
 	@Override
