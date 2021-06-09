9aad630a5bc483cf6099cb210e20831a118ffe3d
edbwl <edbwl@LAPTOP-V66A7OUD>
2021-06-10 04:10:11 +0900

    메인 수정

--- src/main/umMain/UmbrellaMain.java
+++ src/main/umMain/UmbrellaMain.java
@@ -32,6 +32,11 @@ public class UmbrellaMain extends JFrame implements ActionListener {
 	private JPanel panelTitle, panelBtn;
 	private JButton btnRental, btnAdmin, btnReturn;
 	private JLabel lblTitle, lblSubTitle;
+	private JButton btnStats;
+	private JPanel panelTop;
+	private JPanel panelExit;
+	private JButton btnExit;
+	private JPanel panelAll;
 
 	public UmbrellaMain(String title, int width, int height) {
 		setTitle(title);
@@ -39,24 +44,69 @@ public class UmbrellaMain extends JFrame implements ActionListener {
 		setLocationRelativeTo(this); // 현재 클래스에 대해서 상대적인 위치
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		// 타이틀 생성
-		addTitle();
+		// 전체 패널(테두리를 주기 위함)
+		panelAll = new JPanel();
+		panelAll.setLayout(new BorderLayout());
+		panelAll.setBorder(new LineBorder(Color.GRAY, 2));	//테두리
+		
+		// 탑 패널 생성
+		addPanelTop();
 
 		// 버튼 생성
 		addBtn();
 
-		add(panelTitle, BorderLayout.NORTH);
-		add(panelBtn, BorderLayout.CENTER);
-		//setUndecorated(true);	//타이틀 없애기
+		panelAll.add(panelTop, BorderLayout.NORTH);
+		panelAll.add(panelBtn, BorderLayout.CENTER);
+		add(panelAll);
+		
+		setUndecorated(true); //타이틀 없애기
 		setVisible(true);
 	}
 
-	
 	// 타이틀 생성
-	private void addTitle() {
+	private void addPanelTop() {
 
+		panelTop = new JPanel();
+		panelTop.setLayout(new BorderLayout());
+
+		// 나가기 버튼
+		addExit();
+
+		// 타이틀
+		addTitle();
+
+		panelTop.add(panelExit, BorderLayout.NORTH);
+		panelTop.add(panelTitle, BorderLayout.CENTER);
+	}
+
+	// 나가기 버튼
+	private void addExit() {
+
+		panelExit = new JPanel();
+		panelExit.setLayout(new FlowLayout(FlowLayout.RIGHT));
+		panelExit.setBorder(BorderFactory.createEmptyBorder(5, 0, 0, 5));
+		panelExit.setBackground(Color.WHITE);
+
+		// 이미지
+		ImageIcon iconExit = new ImageIcon("libs/exit.png");
+		Image changeIcon = iconExit.getImage().getScaledInstance(30, 30, Image.SCALE_SMOOTH);
+		ImageIcon btnExitIcon = new ImageIcon(changeIcon);
+
+		// 버튼
+		btnExit = new JButton(btnExitIcon);
+		btnExit.addActionListener(this);
+		btnExit.setBackground(Color.WHITE);
+		btnExit.setBorderPainted(false);
+
+		btnExit.addActionListener(this);
+		panelExit.add(btnExit);
+
+	}
+	
+	// 타이틀
+	private void addTitle() {
 		panelTitle = new JPanel();
-		panelTitle.setLayout(new GridLayout(2, 1, 0, 10));
+		panelTitle.setLayout(new GridLayout(2, 1, 0, 0));
 		panelTitle.setBackground(new Color(0xFFFFFF)); // Color rgb 값 가져와 배경색 설정
 		panelTitle.setBorder(BorderFactory.createEmptyBorder(130, 0, 0, 0)); // 마진 설정
 
@@ -75,40 +125,39 @@ public class UmbrellaMain extends JFrame implements ActionListener {
 		lblSubTitle.setFont(fontSubTitle); // 설정한 font 적용
 
 		panelTitle.add(lblSubTitle);
-
 	}
 
-	
-	//버튼 생성
+	// 버튼 생성
 	private void addBtn() {
 
 		panelBtn = new JPanel();
 		panelBtn.setLayout(new GridLayout(1, 3, 10, 10));
 		panelBtn.setBackground(new Color(0xFFFFFF)); // 패널 배경색 설정
-		panelBtn.setBorder(BorderFactory.createEmptyBorder(0, 100, 0, 100)); // 마진 설정
+		panelBtn.setBorder(BorderFactory.createEmptyBorder(0, 30, 0, 30)); // 마진 설정
 
 		// 이미지 생성 및 크기 설정
 		ImageIcon icon = new ImageIcon("libs/umbrella.png");
-		Image changeIcon = icon.getImage().getScaledInstance(110, 110, Image.SCALE_SMOOTH);
+		Image changeIcon = icon.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH);
 		ImageIcon btnIcon = new ImageIcon(changeIcon);
 
 		// 버튼 글자 스타일
-		Font fontBtn = new Font("HY헤드라인M", Font.PLAIN, 20);
+		Font fontBtn = new Font("HY헤드라인M", Font.PLAIN, 21);
 
 		// 버튼 생성
 		btnRental = new JButton("대여", btnIcon);
 		btnRental.addActionListener(this);
-		
+
 		btnReturn = new JButton("반납", btnIcon);
 		btnReturn.addActionListener(this);
 
 		btnAdmin = new JButton("관리자", btnIcon);
 		btnAdmin.addActionListener(this);
-		
-		
-		
+
+		btnStats = new JButton("분석", btnIcon);
+		btnStats.addActionListener(this);
+
 		// 버튼에 스타일 적용
-		JButton[] btnArr = { btnRental, btnReturn, btnAdmin };
+		JButton[] btnArr = { btnRental, btnReturn, btnAdmin, btnStats };
 
 		for (int i = 0; i < btnArr.length; i++) {
 
@@ -123,6 +172,7 @@ public class UmbrellaMain extends JFrame implements ActionListener {
 		panelBtn.add(btnRental);
 		panelBtn.add(btnReturn);
 		panelBtn.add(btnAdmin);
+		panelBtn.add(btnStats);
 
 	}
 
@@ -134,15 +184,15 @@ public class UmbrellaMain extends JFrame implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		Object obj = e.getSource();
-		if(obj == btnRental) {
+		if (obj == btnRental) {
 			new Rental("대여", 900, 600);
-		}
-		else if (obj == btnReturn) {
+		} else if (obj == btnReturn) {
 			new Return("반납", 900, 600);
-		}
-		else if(obj == btnAdmin) {
+		} else if (obj == btnAdmin) {
 			new Admin("관리자", 900, 700);
+		} else if(obj == btnExit) {
+			System.exit(0);
 		}
-		
+
 	}
 }
