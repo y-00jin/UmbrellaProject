c9691911338904ff53cb629d518e3b6bf6ca3ca3
0206cho <68586044+0206cho@users.noreply.github.com>
2021-06-13 22:11:23 +0900

    대여상태 조건

--- src/main/umRental/Rental_ModifyBtn.java
+++ src/main/umRental/Rental_ModifyBtn.java
@@ -121,8 +121,13 @@ public class Rental_ModifyBtn extends JFrame implements ActionListener {
 			String umcode = tf_Umbcode.getText();
 			String getId = rental.getRentalId();
 			
-//미안하다,,, 미래의 김민솔 중복 힘내라	
-			
+			//미안하다,,, 미래의 김민솔 중복 힘내라	
+			// 미반납자에 있는 학번, 현재 대여중인 학번, 이미 대여중인 우산아이디
+			// 대여를 하면 반납 상태가 N, 반납을 하면 Y
+			// 이미 반납한 학생은 반납상태가 Y,
+			// 현재 대여중(+미반납자)이면 지금 대여 테이블에 학번이 있으니까
+			// 중복 처리할때 반납상태가 Y이거나 대여테이블에 학번이 없을경우로 하면 될듯
+			// while문으로 돌려야 하나,,,
 			
 			if (tf_Code.getText() == "1"/* 사실 1은 아니고 만약 중복된다면 */) {
 				JOptionPane.showMessageDialog( // 메시지창 출력
